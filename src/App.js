d4b981194c2a358b5561250b989e560e834b809d
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-15 11:04:30 +0100

    exo finis

--- src/App.js
+++ src/App.js
@@ -1,1 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import './App.css'
+
+export class App extends Component {
+
+  state = {
+    span: 0
+  }
+
+  plus = () => {
+    this.setState({
+      span: this.state.span += 1
+    })
+  }
+
+  moins = () => {
+    this.setState({
+      span: this.state.span -= 1
+    })
+  }
+
+  rouge = () => {
+    document.querySelector("span").classList.toggle("rouge")
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.span}</span>
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.moins}>-</button>
+        <button onClick={this.rouge}>Color</button>
+      </div>
+    )
+  }
+}
+
+export default App
